9431d283bd6be9b2ba633c97ba091d0ff2868ce6
bre 97 <by.sgatn3@Gmail.com>
2024-03-12 18:54:45 +0800

    chore(config): update tsconfig, create rollup config

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+// rollup.config.mjs
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: './build/index.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: './build/index.esm.js',
+      format: 'es',
+    },
+  ],
+  plugins: [nodeResolve(), typescript({tsconfig: './tsconfig.json'})],
+};
