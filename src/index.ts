ee2eceb258b983180651ca59f2736972a830ea9b
bre 97 <by.sgatn3@Gmail.com>
2024-03-16 17:52:30 +0800

    fix(src): update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+export * from './color/contrast';
+export * from './color/default-colors';
+export {TColor, TMaterialColors} from './color/material-colors';
+export * from './generator/IMaterialGenerator';
+export * from './generator/IStylizable';
 export * from './generator/MaterialDynamicSchemeGenerator';
 export * from './generator/MaterialPaletteGenerator';
-export * from './color/default-colors';
+export * from './utils/strings';
