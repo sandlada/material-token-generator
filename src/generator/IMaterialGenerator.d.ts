b45006fda3accea7635aa37d34a28b67486e6281
bre 97 <by.sgatn3@Gmail.com>
2024-03-16 16:14:38 +0800

    feat(color): EVariant, TVariant

--- src/generator/IMaterialGenerator.d.ts
+++ src/generator/IMaterialGenerator.d.ts
@@ -1,8 +1,27 @@
 import {TColor} from '../color/material-colors';
 
+export type TVariant = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+export const enum EVariant {
+  MONOCHROME = 0,
+  NEUTRAL = 1,
+  TONAL_SPOT = 2,
+  VIBRANT = 3,
+  EXPRESSIVE = 4,
+  FIDELITY = 5,
+  CONTENT = 6,
+  RAINBOW = 7,
+  FRUIT_SALAD = 8,
+}
+
 export type TMaterialGeneratorOptions = {
   isDark: boolean;
   contrastLevel: number | TMaterialColorContrastLevel;
+  primaryPalette: TColor;
+  secondaryPalette: TColor;
+  tertiaryPalette: TColor;
+  neutralPalette: TColor;
+  neutralVariantPalette: TColor;
+  variant: TVariant;
 };
 
 export interface IMaterialGenerator<TI extends Object, TR> {
