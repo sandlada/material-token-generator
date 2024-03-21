974e5a06f7986b635b33244b2b03d8fff2f70dd7
bre 97 <by.sgatn3@Gmail.com>
2024-03-21 18:54:48 +0800

    chore(config): update config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,18 +1,19 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
 
 // rollup.config.mjs
 export default {
   input: './src/index.ts',
   output: [
     {
-      file: './build/index.cjs.js',
+      file: './build/index.cjs',
       format: 'cjs',
     },
     {
-      file: './build/index.esm.js',
+      file: './build/index.mjs',
       format: 'es',
     },
   ],
-  plugins: [nodeResolve(), typescript({ tsconfig: './tsconfig.json' })],
+  plugins: [nodeResolve(), typescript({tsconfig: './tsconfig.json'}), terser()],
 };
