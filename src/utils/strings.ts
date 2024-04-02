2c48dfd5ee068a6f84b82ba0a49a82a1e174208a
bre 97 <by.sgatn3@Gmail.com>
2024-04-02 17:51:24 +0800

    fix: fix FromColorStringToInt can not parse rgb

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -16,9 +16,12 @@ export function ToKebabCase(str: string) {
 }
 
 export function FromColorStringToInt(sourceColor: string): number {
+  // #123456
   if (sourceColor.includes('#')) {
     return argbFromHex(sourceColor);
-  } else if (sourceColor.includes('rgba(')) {
+  } 
+  // rgba(1, 2, 3, 0)
+  else if (sourceColor.includes('rgba(')) {
     const rgb = (
       sourceColor
         .replace('rgba(', '')
@@ -26,10 +29,12 @@ export function FromColorStringToInt(sourceColor: string): number {
         .split(',') as Array<string>
     ).map(s => parseInt(s));
     return argbFromRgba({r: rgb[0], g: rgb[1], b: rgb[2], a: rgb[3]});
-  } else if (sourceColor.includes('rgb(')) {
+  }
+  // rgb(1, 2, 3)
+  else if (sourceColor.includes('rgb(')) {
     const rgb = (
       sourceColor
-        .replace('rgba(', '')
+        .replace('rgb(', '')
         .replace(')', '')
         .split(',') as Array<string>
     ).map(s => parseInt(s));
