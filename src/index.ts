ac67b39452017ffa179a8b1c4d14a9d624f3c04e
bre97-web <bre97-web@outlook.com>
2024-10-03 13:14:42 +0800

    chore: export material-dynamic-palettes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export * from './color/material-contrast-level'
 export * from './color/material-variant'
 
+export * from './core/material-dynamic-palettes'
 export * from './core/material-dynamic-tokens'
 export * from './core/material-palettes'
 export * from './core/material-tokens'
 
+export * from './material-colors/material-colors'
